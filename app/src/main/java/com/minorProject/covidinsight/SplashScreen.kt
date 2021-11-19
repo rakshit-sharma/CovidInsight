49c123c23f1d33bbabbc8adc5a98e8f5145ce1fa
Rakshit Sharma <rakshitsharma2001@hotmail.com>
2021-11-20 02:45:42 +0530

    2nd commit

---
+++ app/src/main/java/com/minorProject/covidinsight/SplashScreen.kt
@@ -0,0 +1,30 @@
+package com.minorProject.covidinsight
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        window.setFlags(
+
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+
+        Handler().postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 3000)
+    }
+}
