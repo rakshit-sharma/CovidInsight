756c481f331a450424a97ede39bb4f434bd48bde
Rakshit Sharma <rakshitsharma2001@hotmail.com>
2021-11-26 17:00:09 +0530

    2nd commit

---
+++ app/src/main/java/com/minorProject/covidinsight/CovidData.kt
@@ -0,0 +1,39 @@
+package com.minorProject.covidinsight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import okhttp3.*
+import java.io.IOException
+
+class CovidData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_covid_data)
+        val textView : TextView = findViewById(R.id.api_name)
+        textView.text = "Hello"
+
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("https://free-nba.p.rapidapi.com/players?page=0&per_page=25")
+            .get()
+            .addHeader("x-rapidapi-host", "free-nba.p.rapidapi.com")
+            .addHeader("x-rapidapi-key", "c0d0ac06d1msh357a33595b3e794p1ecb7cjsn84d779d2e89d")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                Log.d("MyTAG", "onCreate: "+response.body?.string())
+                this@CovidData.runOnUiThread(Runnable {
+                    textView.text = response.body?.string()
+                }
+                )
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("MyTAG", "onCreate: "+e)
+            }
+        })
+    }
+}
